517e1387ef9ffd48f82b2f1b5ea34a1c0e6e398e
Michael Parque <Michael Parque>
2017-02-21 17:31:42 -0500

    Added ciphertext and finished decode method.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace ConsoleApplication
 {
     public class Program
     {
+        private static readonly Dictionary<char, char> outback_map = new Dictionary<char, char>
+        {
+            {'a', 'z'},
+            {'b', 'y'},
+            {'c', 'x'},
+            {'d', 'w'},
+            {'e', 'v'},
+            {'f', 'u'},
+            {'g', 't'},
+            {'h', 's'},
+            {'i', 'r'},
+            {'j', 'q'},
+            {'k', 'p'},
+            {'l', 'o'},
+            {'m', 'n'},
+            {'n', 'm'},
+            {'o', 'l'},
+            {'p', 'k'},
+            {'q', 'j'},
+            {'r', 'i'},
+            {'s', 'h'},
+            {'t', 'g'},
+            {'u', 'f'},
+            {'v', 'e'},
+            {'w', 'd'},
+            {'x', 'c'},
+            {'y', 'b'},
+            {'z', 'a'}
+        };
         public static void Main(string[] args)
         {
-            string input = "Hello world!";
-            string output = DecodeOutbackCipher(input);
-            Console.WriteLine(output);
+            string filename = args[0];
+            using (StreamReader sr = File.OpenText(filename))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(DecodeOutbackCipher(s));
+                }
+            }
         }
 
         /// <summary>
@@ -20,7 +58,13 @@ namespace ConsoleApplication
         /// <returns></returns>
         public static string DecodeOutbackCipher(string input)
         {
-            return input;
+            input = input.ToLower();
+            var output_buf = new StringBuilder();
+            foreach (char c in input)
+            {
+                output_buf.Append(outback_map.ContainsKey(c) ? outback_map[c] : c);
+            }
+            return output_buf.ToString();
         }
     }
 }
