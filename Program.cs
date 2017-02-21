0a6e21ad7c4cafc259e7ee796903c98dbfef7f41
Michael Parque <Michael Parque>
2017-02-21 14:43:31 -0500

    Initial commit and setup of project.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = "Hello world!";
+            string output = DecodeOutbackCipher(input);
+            Console.WriteLine(output);
+        }
+
+        /// <summary>
+        /// Decode given string encoded with outback cipher.
+        /// 
+        /// Outback cipher is a reflection performed on each letter. e.g. 'A' -> 'Z', 'B' -> 'Y'
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string DecodeOutbackCipher(string input)
+        {
+            return input;
+        }
+    }
+}
